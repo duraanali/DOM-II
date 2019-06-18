92a730a845fd308e4b8ee4d41aa263269877d53b
Duraan Ali <duraan522@gmail.com>
2019-06-18 15:48:55 -0500

    Final change to index js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-// Your code goes here
+// // Your code goes here
 
-//---------Mouse over the logo -------------
+//---------1. Mouse over the logo -------------
 let logo = document.querySelector(".logo-heading");
 logo.addEventListener("mouseover", function (event) { 
     event.stopPropagation();
+    event.preventDefault();
     event.target.style.color = "blue"; 
 
     setTimeout(function () {
@@ -11,16 +12,17 @@ logo.addEventListener("mouseover", function (event) {
 }, 500);
 }, false);
 
-//----------- Hit Enter to make main image disappear --------
+//-----------2. Hit Enter to make main image disappear --------
 
 document.addEventListener('keydown', function (event) {
     event.stopPropagation();
+    event.preventDefault();
     if (event.code == 'Enter') {
         document.querySelector(".intro").style.display = "none";
     }
 });
 
-//----------------Zoom in Zoom Out middle right image--------------
+//----------------3. Zoom in Zoom Out middle right image--------------
 
 function zoom(event) {
     event.stopPropagation();
@@ -39,10 +41,74 @@ let scale = 1;
 const firstImg = document.querySelector('.img-content');
 firstImg.onwheel = zoom;
 
-//----------------
+//----------------4. Alert message for onload --------------
 
 window.addEventListener('load', (event) => {
     event.stopPropagation();
+    event.preventDefault();
     alert('The Website just started to load!');
     
+});
+
+//-------------- 5. Double Click----------
+
+
+const doubleClick = document.querySelector('body')
+
+doubleClick.addEventListener("dblclick", function (eventObject) {
+    event.stopPropagation();
+    event.preventDefault();
+    alert(`Hey, You just Double clicked!`);
+});
+
+//-------------- 6. Click----------
+const buttonClick = document.querySelector('.btn');
+
+buttonClick.addEventListener('click', event => {
+    event.stopPropagation();
+    event.preventDefault();
+    buttonClick.innerHTML = `Click More ${event.detail}`;
+});
+
+//----------- 7. MouseOver
+const pageImages = document.querySelectorAll('img');
+
+pageImages.forEach(image => image.addEventListener('mouseover', event => {
+    event.target.style.border = '10px solid black';
+}));
+
+//------------8. Before Print
+
+window.addEventListener("beforeprint", myFunction);
+
+function myFunction() {
+    event.stopPropagation();
+    event.preventDefault();
+    alert("Hey, You cant print this page, JK!");
+}
+
+//-------------9. Before Leaving
+
+window.addEventListener("beforeunload", function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    event.returnValue = "Write something clever here..";
+});
+
+//---------------10.
+
+document.querySelector("nav").addEventListener("contextmenu", myFunction);
+
+function myFunction() {
+    event.stopPropagation();
+    event.preventDefault();
+    var x = document.querySelector("nav");
+    x.innerHTML = "OOPS, You just right clicked!";
+    x.style.fontSize = "30px";
+}
+
+
+document.querySelector("nav a").addEventListener("click", function (event) {
+    event.stopPropagation();
+    event.preventDefault()
 });
