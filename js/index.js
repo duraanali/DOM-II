5261ec534d47f7ba4dcab0ad999545e38c328419
Duraan Ali <duraan522@gmail.com>
2019-07-09 14:59:25 -0500

    Added the stretch goal

--- js/index.js
+++ js/index.js
@@ -2,97 +2,101 @@
 
 //---------1. Mouse over the logo -------------
 let logo = document.querySelector(".logo-heading");
-logo.addEventListener("mouseover", function (event) { 
+logo.addEventListener(
+  "mouseover",
+  function(event) {
     event.stopPropagation();
     event.preventDefault();
-    event.target.style.color = "blue"; 
+    event.target.style.color = "blue";
 
-    setTimeout(function () {
-    event.target.style.color = "";
-}, 500);
-}, false);
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false
+);
 
 //-----------2. Hit Enter to make main image disappear --------
 
-document.addEventListener('keydown', function (event) {
-    event.stopPropagation();
-    event.preventDefault();
-    if (event.code == 'Enter') {
-        document.querySelector(".intro").style.display = "none";
-    }
+document.addEventListener("keydown", function(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  if (event.code == "Enter") {
+    document.querySelector(".intro").style.display = "none";
+  }
 });
 
 //----------------3. Zoom in Zoom Out middle right image--------------
 
 function zoom(event) {
-    event.stopPropagation();
-    event.preventDefault();
+  event.stopPropagation();
+  event.preventDefault();
 
-    scale += event.deltaY * -0.01;
+  scale += event.deltaY * -0.01;
 
-    // Restrict scale
-    scale = Math.min(Math.max(.125, scale), 4);
+  // Restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
 
-    // Apply scale transform
-    firstImg.style.transform = `scale(${scale})`;
+  // Apply scale transform
+  firstImg.style.transform = `scale(${scale})`;
 }
 
 let scale = 1;
-const firstImg = document.querySelector('.img-content');
+const firstImg = document.querySelector(".img-content");
 firstImg.onwheel = zoom;
 
 //----------------4. Alert message for onload --------------
 
-window.addEventListener('load', (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-    alert('The Website just started to load!');
-    
+window.addEventListener("load", event => {
+  event.stopPropagation();
+  event.preventDefault();
+  alert("The Website just started to load!");
 });
 
 //-------------- 5. Double Click----------
 
+const doubleClick = document.querySelector("body");
 
-const doubleClick = document.querySelector('body')
-
-doubleClick.addEventListener("dblclick", function (eventObject) {
-    event.stopPropagation();
-    event.preventDefault();
-    alert(`Hey, You just Double clicked!`);
+doubleClick.addEventListener("dblclick", function(eventObject) {
+  event.stopPropagation();
+  event.preventDefault();
+  alert(`Hey, You just Double clicked!`);
 });
 
 //-------------- 6. Click----------
-const buttonClick = document.querySelector('.btn');
+const buttonClick = document.querySelector(".btn");
 
-buttonClick.addEventListener('click', event => {
-    event.stopPropagation();
-    event.preventDefault();
-    buttonClick.innerHTML = `Click More ${event.detail}`;
+buttonClick.addEventListener("click", event => {
+  event.stopPropagation();
+  event.preventDefault();
+  buttonClick.innerHTML = `Click More ${event.detail}`;
 });
 
 //----------- 7. MouseOver
-const pageImages = document.querySelectorAll('img');
+const pageImages = document.querySelectorAll("img");
 
-pageImages.forEach(image => image.addEventListener('mouseover', event => {
-    event.target.style.border = '10px solid black';
-}));
+pageImages.forEach(image =>
+  image.addEventListener("mouseover", event => {
+    event.target.style.border = "10px solid black";
+  })
+);
 
 //------------8. Before Print
 
 window.addEventListener("beforeprint", myFunction);
 
 function myFunction() {
-    event.stopPropagation();
-    event.preventDefault();
-    alert("Hey, You cant print this page, JK!");
+  event.stopPropagation();
+  event.preventDefault();
+  alert("Hey, You cant print this page, JK!");
 }
 
 //-------------9. Before Leaving
 
-window.addEventListener("beforeunload", function (event) {
-    event.stopPropagation();
-    event.preventDefault();
-    event.returnValue = "Write something clever here..";
+window.addEventListener("beforeunload", function(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  event.returnValue = "Write something clever here..";
 });
 
 //---------------10.
@@ -100,15 +104,16 @@ window.addEventListener("beforeunload", function (event) {
 document.querySelector("nav").addEventListener("contextmenu", myFunction);
 
 function myFunction() {
-    event.stopPropagation();
-    event.preventDefault();
-    var x = document.querySelector("nav");
-    x.innerHTML = "OOPS, You just right clicked!";
-    x.style.fontSize = "30px";
+  event.stopPropagation();
+  event.preventDefault();
+  var x = document.querySelector("nav");
+  x.innerHTML = "OOPS, You just right clicked!";
+  x.style.fontSize = "30px";
 }
 
-
-document.querySelector("nav a").addEventListener("click", function (event) {
-    event.stopPropagation();
-    event.preventDefault()
+document.querySelector("nav a").addEventListener("click", function(event) {
+  event.stopPropagation();
+  event.preventDefault();
 });
+
+TweenMax.to("img", 5, { y: 20 });
